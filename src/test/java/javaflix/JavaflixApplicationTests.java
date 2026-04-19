e2f24b8bb70ba4df974fbc845ff827d2daba6298
Lucas Caldardo <llucascalldardo@gmail.com>
2026-04-19 18:13:14 -0300

    first commit

---
+++ src/test/java/javaflix/JavaflixApplicationTests.java
@@ -0,0 +1,13 @@
+package javaflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
