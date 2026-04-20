3b2b89b496035ac600e29060ae4697c52aa9dc26
Lucas Caldardo <llucascalldardo@gmail.com>
2026-04-20 14:58:55 -0300

    feat: Cria a classe CategoriaController

--- src/main/java/javaflix/controller/CategoriaController.java
+++ src/main/java/javaflix/controller/CategoriaController.java
@@ -1,4 +1,45 @@
 package javaflix.controller;
 
+import javaflix.entity.Categoria;
+import javaflix.service.CategoriaService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/javaflix/categoria")
+@RequiredArgsConstructor
 public class CategoriaController {
+
+
+    private final CategoriaService categoriaService;
+
+
+    @GetMapping
+    public List<Categoria> TodasCategorias(){
+        return categoriaService.TodasCategorias();
+    }
+
+    @PostMapping
+    public Categoria criarCategoria(@RequestBody Categoria categoria){
+        return categoriaService.criarCategoria(categoria);
+    }
+
+    @GetMapping("/{id}")
+    public Categoria listarPorId(@PathVariable Long id){
+        Optional<Categoria> optCategoria = categoriaService.listarPorId(id);
+        if (optCategoria.isPresent()){
+            return optCategoria.get();
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarPorId(@PathVariable Long id) {
+        categoriaService.deletarPorId(id);
+    }
 }
---
+++ src/main/resources/db/migration/V1__create_table_categoria.sql
@@ -0,0 +1,4 @@
+CREATE TABLE CATEGORIA (
+    id serial PRIMARY KEY,
+    nome varchar(100) NOT NULL
+);
