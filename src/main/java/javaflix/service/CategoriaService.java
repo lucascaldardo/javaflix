37dd5a3ebadc2187a8f6625ab3ed01004a2c90df
Lucas Caldardo <llucascalldardo@gmail.com>
2026-04-20 14:58:26 -0300

    feat: Cria a classe CategoriaService

--- src/main/java/javaflix/service/CategoriaService.java
+++ src/main/java/javaflix/service/CategoriaService.java
@@ -1,4 +1,36 @@
 package javaflix.service;
 
+import jakarta.persistence.Id;
+import jakarta.validation.OverridesAttribute;
+import javaflix.entity.Categoria;
+import javaflix.repository.CategoriaRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class CategoriaService {
+
+    private final CategoriaRepository repository;
+
+    public List<Categoria> TodasCategorias(){
+        return repository.findAll();
+    }
+
+    public Categoria criarCategoria(Categoria categoria){
+        return repository.save(categoria);
+    }
+
+    public Optional<Categoria> listarPorId(@PathVariable Long id){
+        return repository.findById(id);
+    }
+
+    public void deletarPorId(@PathVariable Long id){
+        repository.deleteById(id);
+    }
 }
